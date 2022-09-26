74e0501964dee43b5fa684439b67d6ff302c7165
thais <trevillon@jehann.fr>
2022-09-26 14:31:03 +0200

    :tada: navigation et router

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,17 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import {PresentationComponent} from "./pages/presentation/presentation.component";
+import {DirectivesComponent} from "./pages/directives/directives.component";
+import {NotfoundComponent} from "./pages/notfound/notfound.component";
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', component: PresentationComponent},
+  {path: 'directives', component: DirectivesComponent},
+  {path: 'accueil', redirectTo: ''},
+  {path: 'acceuil', redirectTo: '' },
+  {path: '**', component: NotfoundComponent}, // se met en dernier
+];
+
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,17 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { PresentationComponent } from './pages/presentation/presentation.component';
+import { DirectivesComponent } from './pages/directives/directives.component';
+import { NotfoundComponent } from './pages/notfound/notfound.component';
+import { NavigationComponent } from './components/navigation/navigation.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    PresentationComponent
+    PresentationComponent,
+    DirectivesComponent,
+    NotfoundComponent,
+    NavigationComponent
   ],
   imports: [
     BrowserModule,
